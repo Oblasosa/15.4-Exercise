3aff22e9fbfd52c3c5c0241f9983c53c01186481
Steven <cow3298@gmail.com>
2023-02-08 14:42:37 -0800

    fill in functions

--- 15.4-Exercises.js
+++ 15.4-Exercises.js
@@ -82,28 +82,33 @@ const extend = (arr1, arr2) => {
 //     from obj and a new key/value pair 
 
 const addKeyVal = (obj, key, val) => {
-    return obj.pop(key, val)
+    let obj2  = {...obj}
+    obj2[key] = val;
+    return obj2;
 };
 
 
 //     Return a new object with a key removed. 
 
-//     function removeKey(obj, key) {
-
-//     }
+const removeKey = (obj, key) => {
+  let obj2 = {...obj}
+  delete obj2[key];
+  return obj2;
+};
 
 
 //     Combine two objects and return a new object. 
 
-//     function combine(obj1, obj2) {
-
-//     }
+const combine = (obj1, obj2) => {
+  return {...obj1, ...obj2};
+};
 
 
 //     Return a new object with a modified key and value. 
-
-//     function update(obj, key, val) {
-
-//     }
+const update = (obj, key, val) => {
+  let obj2 = {...obj}
+  obj2[key] = val;
+  return obj2;
+};
 
 
